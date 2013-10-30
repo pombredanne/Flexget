29768b0a9cb46b28fd981517476f229ee9818e30
Toilal <toilal.dev@gmail.com>
2013-10-30 08:23:12 +0100

    And parser language methods

--- flexget/utils/titles/parser.py
+++ flexget/utils/titles/parser.py
@@ -1,6 +1,6 @@
 from __future__ import unicode_literals, division, absolute_import
 import re
-
+import pycountry
 
 class ParseWarning(Warning):
 
@@ -27,6 +27,22 @@ class TitleParser(object):
     remove = ['imax']
 
     sounds = ['AC3', 'DD5.1', 'DTS']
+        
+    __languages_name = {'VF', pycountry.languages.get(alpha2='fr')}    
+    __languages_code = {}
+    __countries_code = {}
+    __countries_name = {}
+    
+    for lang in pycountry.languages:
+        __languages_code[lang.bibliographic.lower()] = lang
+        __languages_name[lang.name.lower()] = lang
+        __languages_name["true" + lang.name.lower()] = lang
+        
+    for country in pycountry.countries:
+        __countries_code[country.alpha3.lower()] = country
+        __countries_code[country.alpha2.lower()] = country
+        __countries_name[country.name.lower()] = country
+        __countries_name["true" + country.name.lower()] = country
 
     @staticmethod
     def re_not_in_word(regexp):
@@ -54,3 +70,30 @@ class TitleParser(object):
             old = TitleParser.re_not_in_word(old)
         pattern = re.compile(old, re.I)
         return re.sub(pattern, new, data, count)
+    
+    @staticmethod
+    def language_from_name(word):
+        """Retrieves language from name word
+        
+        :param word: the word to test ('English', 'French', 'German', 'TrueFrench', ...)
+        :return: the pycountry.db.Language object, or None if word is not a language name
+        """
+        lword = word.lower()
+        lang = TitleParser.__languages_name.get(lword)
+        if not lang:
+            lang = TitleParser.__countries_name.get(lword)
+        return lang
+    
+    @staticmethod
+    def language_from_code(word):
+        """Retrieves language from code word
+        
+        :param word: the word to test ('en', 'fr', 'ge', ...)
+        :return: the pycountry.db.Language object, or None if word is not a language name
+        """
+        lword = word.lower()
+        lang = TitleParser.__languages_code.get(lword)
+        if not lang:
+            lang = TitleParser.__countries_code.get(lword)
+        return lang
+    
